f81f62ce8b74e10cefe46201564fdb1aaa937ce7
Thijs Raymakers <thijs@raymakers.nl>
2017-09-16 23:23:52 +0200

    Restored the needs more jpeg command

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ async def on_ready():
     print('------')
 
 
-@bot.command(description='For when you wanna settle the score some other way')
+@bot.command(description='Picks one choice from the arguments you used')
 async def choose(*choices : str):
     """Chooses between multiple choices."""
     await bot.say(random.choice(choices))
 
 
-@bot.command(description='For when you want the bot to repeat something (Max:5)')
+@bot.command(description='Repeat a word multiple times (Maximum of 5 times)')
 async def repeat(times : int, content='repeating...'):
     """Repeats a message multiple times."""
     if times > 5:
@@ -31,34 +31,23 @@ async def repeat(times : int, content='repeating...'):
         await bot.say(content)
 
 
-@bot.command(description='Tells you when someone joined discord')
+@bot.command(description='Returns the exact time when the user joined the discord server')
 async def joined(member : discord.Member):
     """Says when a member joined."""
     await bot.say('{0.name} joined in {0.joined_at}'.format(member))
 
 
-@bot.command(pass_context=True, description='Adds more jpeg and shrinks')
-async def needsmorejpeg(context, url: str, times: int):
-    """Adds a bit more JPEG to the url"""
-    if times is None:
-        times = 0
-    elif times < 0:
-        times = 0
-    elif times > 4:
-        times = 4   
-    
+@bot.command(pass_context=True, description='Sets the JPEG quality to 1')
+async def needsmorejpeg(context, url):
+    """Adds a bit more JPEG"""
     channel = context.message.channel
-    await nmj.compress_img(await nmj.url_to_img(url), 'temp.jpeg', times)
+    await nmj.compress_img(await nmj.url_to_img(url), 'temp.jpeg')
     await bot.send_file(channel, 'temp.jpeg')
-
-
-@bot.command()
-async def anthem():
-    await bot.say('https://cdn.discordapp.com/attachments/357261287740145665/358184959728418816/andy_jesus.mp4')
 
 
 @bot.group()
 async def cool(name: str):
+    """Check if things are cool"""
     if name.lower() == 'bot':
         await bot.say('Yes, I\'m quite cool.')
     elif name.lower() == 'andy':
--- needs_more_jpeg.py
+++ needs_more_jpeg.py
@@ -10,7 +10,4 @@ async def url_to_img(url):
 
 async def compress_img(img, filename, times):
     img = img.convert("RGB")
-    ratio = math.pow(2 , times)
-    if times is not None:
-        img.resize((img.size[0]*ratio,img.size[1]*ratio), Image.ANTIALIAS)
     img.save(filename, 'JPEG', quality=1)
