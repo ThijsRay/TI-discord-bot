a64599bec84a5489b01ba319aab09b13b60ea94b
Thijs Raymakers <thijs@raymakers.nl>
2017-09-16 23:04:03 +0200

    Fixed syntax error in needs_more_jpeg.py

--- needs_more_jpeg.py
+++ needs_more_jpeg.py
@@ -10,7 +10,7 @@ async def url_to_img(url):
 
 async def compress_img(img, filename, times):
     img = img.convert("RGB")
-    ratio = math.pow(2 , times);
-    if !(times == None):
+    ratio = math.pow(2 , times)
+    if times is not None:
         img.resize((img.size[0]*ratio,img.size[1]*ratio), Image.ANTIALIAS)
     img.save(filename, 'JPEG', quality=1)
